43e29b0915b0d254dc5ede3ffbac6bda6ceb3f0c
George Zygmund <zygmundgw@cardiff.ac.uk>
2022-11-01 20:31:27 +0000

    Navbar improved and app deployed on Firebase

--- organic-shop2/src/environments/environment.prod.ts
+++ organic-shop2/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'organic-e6541',
+    appId: '1:725204238509:web:1b884ec36c05d5d3dbc624',
+    storageBucket: 'organic-e6541.appspot.com',
+    apiKey: 'AIzaSyBUV818KZg8nVef_9SlT99-_nBGZsv400g',
+    authDomain: 'organic-e6541.firebaseapp.com',
+    messagingSenderId: '725204238509',
+  },
   production: true
 };
--- organic-shop2/src/environments/environment.ts
+++ organic-shop2/src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'organic-e6541',
+    appId: '1:725204238509:web:1b884ec36c05d5d3dbc624',
+    storageBucket: 'organic-e6541.appspot.com',
+    apiKey: 'AIzaSyBUV818KZg8nVef_9SlT99-_nBGZsv400g',
+    authDomain: 'organic-e6541.firebaseapp.com',
+    messagingSenderId: '725204238509',
+  },
   production: false
 };
 
