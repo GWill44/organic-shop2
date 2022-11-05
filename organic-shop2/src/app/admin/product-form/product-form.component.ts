97a2f47ac48a568104546ed4b9181d5dc45d722f
GWill44 <georgezygmund@hotmail.com>
2022-11-05 17:02:44 +0000

    Added a product preview display to product form

--- organic-shop2/src/app/admin/product-form/product-form.component.ts
+++ organic-shop2/src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CategoryService} from "../../category.service";
 import {ProductService} from "../../product.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-form',
@@ -10,20 +11,29 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class ProductFormComponent {
   categories$;
-
   form = new FormGroup({
     title: new FormControl('', [Validators.required]),
     price: new FormControl('', [Validators.required, Validators.min(0)]),
     category: new FormControl('', [Validators.required]),
-    imageUrl: new FormControl('',[Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')])
+    imageUrl: new FormControl('',[Validators.required])
+    // imageUrl validator removed until functioning pattern. Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
   })
 
-  constructor(private categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
+
     this.categories$ = this.categoryService.getCategories();
+  }
+
+  ngOnInit() {
+
   }
 
   save(product: Object){
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
   get title(){ return this.form.get('title'); }
