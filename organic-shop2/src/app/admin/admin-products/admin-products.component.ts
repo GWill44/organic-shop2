87fc44c666736fafd16b60a31fa97f5d5aa7c965
GWill44 <georgezygmund@hotmail.com>
2022-11-07 00:59:17 +0000

    Implemented product searchbar feature

--- organic-shop2/src/app/admin/admin-products/admin-products.component.ts
+++ organic-shop2/src/app/admin/admin-products/admin-products.component.ts
@@ -1,25 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from "../../product.service";
+import {Subscription} from "rxjs";
+
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$;
+  products: any[] | undefined;
+  filteredProducts: any[] | undefined;
+  productsSubscription!: Subscription;
 
   constructor(
     private productService: ProductService) {
-
-    this.products$ = this.productService.getAll();
-    this.products$.subscribe(
-      products => console.log(products)
-    );
   }
 
   ngOnInit(): void {
+    this.productsSubscription =
+    this.productService.getAll().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = products;
+    });
+  }
+
+  filter(query: string){
+    this.filteredProducts = (query) ?
+      this.products!.filter((p: any) => p.payload.val().title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy(): void {
+    this.productsSubscription.unsubscribe();
   }
 
 }
