246dc7d96cb12db860c34aa8d3433f7bc300af92
GWill44 <georgezygmund@hotmail.com>
2022-11-14 13:06:45 +0000

    Implemented addToCart()
    Applciation now creates a cart in the local storage and realtime database.

--- organic-shop2/src/app/product-card/product-card.component.ts
+++ organic-shop2/src/app/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Product} from "../models/product";
+import {ShoppingCartService} from "../shopping-cart.service";
 
 @Component({
   selector: 'product-card',
@@ -10,6 +11,19 @@ export class ProductCardComponent {
   @Input('product') product : Product | undefined;
   @Input('show-actions') showActions : Boolean = true;
 
-  constructor() { }
+  constructor(
+    private shoppingCartService: ShoppingCartService
+  ) { }
+
+  addToCart(product: Product){
+    let cartId = localStorage.getItem('cartId');
+    if(!cartId) {
+      this.shoppingCartService.create().then( result => {
+        if (result.key) {
+          localStorage.setItem('cartId', result.key);
+        }
+      })
+    }
+  }
 
 }
