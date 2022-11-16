1d182840d54918e79f63a6e43d1f5aa77c642165
GWill44 <georgezygmund@hotmail.com>
2022-11-16 06:34:46 +0000

    Refactored the addToCart() method

--- organic-shop2/src/app/shopping-cart.service.ts
+++ organic-shop2/src/app/shopping-cart.service.ts
@@ -21,7 +21,6 @@ export class ShoppingCartService {
   private async getOrCreateCartId(): Promise<string> {
     let cartId = localStorage.getItem('cartId');
     if (cartId) return cartId;
-
     let result = await this.create();
     localStorage.setItem('cartId', result.key!);
     return result.key!;
@@ -30,10 +29,7 @@ export class ShoppingCartService {
   async addToCart(product: Product) {
     let cartId = await this.getOrCreateCartId();
     let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key);
-    item$.valueChanges().pipe(take(1)).subscribe((item: any) => {
-      console.log(item);
-      if (item) item$.update({quantity: item.quantity + 1});
-      else item$.set({ product: product, quantity: 1 });
-    });
+    item$.valueChanges().pipe(take(1)).subscribe((item: any) =>
+      item$.update({product: product, quantity: (item?.quantity || 0) + 1}));
   }
 }
