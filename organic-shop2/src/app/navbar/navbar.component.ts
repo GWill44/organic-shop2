e2ab05b7c216e784a69e6a65a5f4586ff2507bac
GWill44 <georgezygmund@hotmail.com>
2022-11-17 14:59:29 +0000

    Navbar displays shoppingcart quantity

--- organic-shop2/src/app/navbar/navbar.component.ts
+++ organic-shop2/src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthService} from "../auth.service";
 import {AppUser} from "../models/app-user";
+import {ShoppingCartService} from "../shopping-cart.service";
 
 @Component({
   selector: 'navbar',
@@ -9,9 +10,19 @@ import {AppUser} from "../models/app-user";
 })
 export class NavbarComponent  {
   appUser: AppUser | undefined;
+  shoppingCartItemCount: number = 0;
 
-  constructor(private auth: AuthService) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser)
+  constructor(
+    private auth: AuthService,
+    private shoppingCartService: ShoppingCartService) {
+  }
+  async ngOnInit(){
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe( (cart: any) => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items) this.shoppingCartItemCount += cart.items[productId].quantity;
+    });
   }
 
   logout() {
